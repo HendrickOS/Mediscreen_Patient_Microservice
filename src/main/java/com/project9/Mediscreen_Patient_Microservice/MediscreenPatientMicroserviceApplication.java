c90730bb1b3a0626c130c51ef6dfd23cd65290da
Hendrick Osseux <osseux.hendrick@gmail.com>
2022-12-28 14:05:47 +0100

    First commit

---
+++ src/main/java/com/project9/Mediscreen_Patient_Microservice/MediscreenPatientMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.project9.Mediscreen_Patient_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenPatientMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenPatientMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project9/Mediscreen_Patient_Microservice/MediscreenPatientMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project9.Mediscreen_Patient_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenPatientMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
