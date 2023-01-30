e2141d448af7c2ef4b73650590b536bf9583b57f
Hendrick Osseux <osseux.hendrick@gmail.com>
2023-01-30 18:01:51 +0100

    EDIT Update Patient work now properly git status! PatientController->ADD showUpdateForm

--- src/main/java/com/project9/Mediscreen_Patient_Microservice/controllers/PatientController.java
+++ src/main/java/com/project9/Mediscreen_Patient_Microservice/controllers/PatientController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,37 +37,27 @@ public class PatientController {
 		return patientService.save(patient);
 	}
 
-//	@GetMapping("/patients/update/{id}")
-//	public Patient showUpdateForm(@PathVariable("id") Integer id) {
-//		return patientService.findById(id);
-//		Patient patient = patientService.findById(id);
-//		model.addAttribute("patient", patient);
-//		return "patient/update";
-//	}
+	@GetMapping("/patients/update/{id}")
+	public Patient showUpdateForm(@PathVariable("id") Integer id, Model model) {
+		return patientService.findById(id);
+	}
 
 	@PostMapping("/patients/update/{id}")
 	public Patient updatePatient(@PathVariable("id") Integer id, @Valid Patient patient, BindingResult result,
 			Model model) {
 		return patientService.save(patient);
-//		if (result.hasErrors()) {
-//			return "patient/update";
-//		}
-//		patient.setId(id);
-//		patientService.save(patient);
-//		model.addAttribute("patient", patientService.findAll());
-//		return "redirect:/patients/list";
 	}
 
-	@DeleteMapping("/patients/delete")
-	public void deletePatient(@PathVariable("id") Integer id) {
-		Patient patient = patientService.findById(id);
-		patientService.delete(patient);
+//	@DeleteMapping("/patients/delete")
+//	public void deletePatient(@PathVariable("id") Integer id) {
+//		Patient patient = patientService.findById(id);
+//		patientService.delete(patient);
 
 //		if (patient != null) {
 //			patientService.delete(patient);
 //		}
 //		model.addAttribute("patient", patientService.findAll());
 //		return "redirect:/patients/list";
-	}
+//	}
 
 }
